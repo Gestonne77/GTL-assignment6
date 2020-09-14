00478db350fd79e5d5b0603315a263874f2b9259
Michael Annor <michaelannor10@gmail.com>
2020-09-14 12:40:35 +0000

    Updates

---
+++ precache-manifest.0754e3e7b834ac7154a0dd9a312bef15.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ecc57b3ce503511e2071a05412f6347",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d79165f9b5eba393cb29",
+    "url": "/static/css/main.0faaeeda.chunk.css"
+  },
+  {
+    "revision": "c8550a42f4e035516de8",
+    "url": "/static/js/2.a9857d7a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a9857d7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d79165f9b5eba393cb29",
+    "url": "/static/js/main.0d807f07.chunk.js"
+  },
+  {
+    "revision": "89ac118f8d664424e2aa",
+    "url": "/static/js/runtime-main.40fd9fda.js"
+  }
+]);
